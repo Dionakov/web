0dc1e40079c9554cd9f21bed408a39ffbb10ca4e
Roch Dionnet <roch.dionnet@equensworldline.com>
2018-04-20 16:08:18 +0000

    improved layout and design

--- src/main/scala/net/rochdion/web/MainServlet.scala
+++ src/main/scala/net/rochdion/web/MainServlet.scala
@@ -5,19 +5,26 @@ import play.twirl.api.Html
 
 class MainServlet extends ScalatraServlet {
 
-  private def renderWithMainLayout(pageTitle: String, main: Html) = layouts.html.layout.render(
+  private def renderWithMainLayout(pageTitle: String, main: Html, currentNavIndex: Int = 0) = layouts.html.layout.render(
       pageTitle = "Home",
       header = views.html.header.render(),
-      nav = views.html.nav.render(),
+      nav = views.html.nav.render(currentNavIndex),
       main,
       footer = views.html.footer.render()
     )
 
-  get("/") {
-    renderWithMainLayout("Home", views.html.home.render())
+  get("/portfolio") {
+    renderWithMainLayout("Portfolio", views.html.portfolio.render(), 3)
+  }
+  get("/cv") {
+    renderWithMainLayout("Curriculum Vitae", views.html.cv.render(), 2)
   }
 
   get("/about") {
-    renderWithMainLayout("About me", views.html.about.render())
+    renderWithMainLayout("About me", views.html.about.render(), 1)
+  }
+
+  get("/") {
+    redirect("/about")
   }
 }
